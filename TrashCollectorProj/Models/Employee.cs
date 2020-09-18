bd89c28aa92dadb53b97e7b2aaeeaeae3a47c144
Kevin Harvey Jr <lordty@gmail.com>
2020-09-18 09:58:05 -0500

    Added_Employee_Properties_&_Display)

--- TrashCollectorProj/Models/Employee.cs
+++ TrashCollectorProj/Models/Employee.cs
@@ -12,7 +12,16 @@ namespace TrashCollectorProj.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Display(Name = "Zipcode")]
+        public string ZipCode { get; set; }
+        [Display(Name = "Pick Up Completed")]
+        public bool PickUpCompleted { get; set; }
+        [Display(Name = "Charge Customer Balance")]
+        public bool ChargeBalance { get; set; }
 
 
         [ForeignKey("IdentityUser")]
